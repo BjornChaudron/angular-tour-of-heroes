6838c8262a365bc4f0608e57eb53a8e6bb63beae
Björn Chaudron <bjornchaudron@gmail.com>
2021-07-09 16:39:18 +0200

    Replaced hero array with observable
    A regular array would not work in a real life situation. Observable will, since it will emit a value whenever it's available. This way the heroes component can initialise correctly without having to wait for the heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
+import {Observable, of} from "rxjs";
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
+import {HEROES} from "./mock-heroes";;
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class HeroService {
   constructor() {
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
