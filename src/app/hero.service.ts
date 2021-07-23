d968a3f1166bd031204d7b133955687d89f99277
Björn Chaudron <bjornchaudron@gmail.com>
2021-07-23 16:24:10 +0200

    Added missing docs for handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -32,6 +32,12 @@ export class HeroService {
     return heroes;
   }
 
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
